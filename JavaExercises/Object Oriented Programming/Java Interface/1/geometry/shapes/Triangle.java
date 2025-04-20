40d92db6386dd6b0034b2e5fe60a786278084109
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-21 00:14:18 +0500

    Create Triangle.java

---
+++ JavaExercises/Object Oriented Programming/Java Interface/1/geometry/shapes/Triangle.java
@@ -0,0 +1,16 @@
+package geometry.shapes;
+
+public class Triangle implements Shape{
+    public double base;
+    public double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return (base * height) / 2;
+    }
+}
