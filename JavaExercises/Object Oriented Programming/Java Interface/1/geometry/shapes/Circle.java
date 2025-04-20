2511893886e561ad2978f432a99855f032f9cea2
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-21 00:13:53 +0500

    Create Circle.java

---
+++ JavaExercises/Object Oriented Programming/Java Interface/1/geometry/shapes/Circle.java
@@ -0,0 +1,14 @@
+package geometry.shapes;
+
+public class Circle implements Shape{
+    public double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
