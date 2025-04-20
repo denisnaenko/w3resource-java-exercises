98f0778f14c37b84999495f1d19b10912a0f6e88
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-21 00:08:35 +0500

    Create Main.java

---
+++ JavaExercises/Object Oriented Programming/Java Interface/1/geometry/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Shape shape;
+
+        shape = new Rectangle(10, 20);
+        System.out.println(shape.getArea());
+
+        shape = new Circle(2.42);
+        System.out.println(shape.getArea());
+
+        shape = new Triangle(20.1, 57.4);
+        System.out.println(shape.getArea());
+    }
+}
