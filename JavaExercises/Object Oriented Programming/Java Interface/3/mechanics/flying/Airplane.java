46561bcbbe755bc44c76ae6ccefa199b7eaf7929
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-21 00:36:01 +0500

    Create Airplane.java

---
+++ JavaExercises/Object Oriented Programming/Java Interface/3/mechanics/flying/Airplane.java
@@ -0,0 +1,8 @@
+package org.example.mechanics.flying;
+
+public class Airplane implements Flyable{
+    @Override
+    public void fly_obj() {
+        System.out.println("Airplane is flying");
+    }
+}
