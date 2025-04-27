9b2ac205d2e4a960ba5fa91cf22a4b9a5217c5cc
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-28 00:38:49 +0500

    Create ReadList.java

---
+++ JavaExercises/Exception Handling/4/ReadList.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class PositiveNumberException extends Exception {
+    public PositiveNumberException(String message) {
+        super(message);
+    }
+}
+
+public class ReadList {
+    public static void main(String[] args) {
+        try {
+            String filePath = "nums.txt";
+            readList(filePath);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void readList(String filePath) throws FileNotFoundException {
+        File fileWithNums = new File(filePath);
+        Scanner scanner = new Scanner(fileWithNums);
+
+        while (scanner.hasNextLine()) {
+            try {
+                if (scanner.nextInt() > 0) {
+                    throw new PositiveNumberException("Positive Number!");
+                }
+            } catch (PositiveNumberException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+}
