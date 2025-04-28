07f4049410f22d4aa5a62c17fa725d8f7f640698
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-29 04:02:24 +0500

    Create Cat.java

---
+++ JavaExercises/Object Oriented Programming/Java Constructor/1/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+    private final String name;
+    private final int age;
+
+    public Cat() {
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
