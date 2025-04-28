7ea48bcbea70bb2df844fe620540880201d122b6
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-29 04:37:53 +0500

    Create Main.java

---
+++ JavaExercises/Object Oriented Programming/Java Constructor/2/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog my_dog = new Dog("Alex", "Brown");
+        System.out.println("My dog name: " + my_dog.name());
+        System.out.println("My dog color: " + my_dog.color());
+    }
+}
