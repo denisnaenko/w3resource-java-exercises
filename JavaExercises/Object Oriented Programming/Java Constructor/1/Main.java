d25d2df1b2a42bf8aafd4b25a619e0ebed364f7d
Denis Naenko <130864695+denisnaenko@users.noreply.github.com>
2025-04-29 04:02:09 +0500

    Create Main.java

---
+++ JavaExercises/Object Oriented Programming/Java Constructor/1/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat default_cat = new Cat();
+        System.out.println("Cat name: " + default_cat.getName());
+        System.out.println("Cat age: " + default_cat.getAge());
+    }
+}
